535671265f21247acab7a08fd7b79905ef945019
DhruvBansal1312 <140554567+DhruvBansal1312@users.noreply.github.com>
2023-08-03 13:34:23 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      this is about component.
+      <p>
+        this is a react based web application to design your Todo list. you can add and delete your 
+        daily Todos tasks in this.
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-dark text-light my-3 fixed-bottom'>
+      <p className='text-center'>Copyright &copy; MyTodosList.com</p>
+    </footer>
+  )
+}
+
+export default Footer
